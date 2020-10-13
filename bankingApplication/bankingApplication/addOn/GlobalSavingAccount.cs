cf9914dae4c82aa1a5b7f88a7fc36c63c01b3538
CrxJr <50372323+CrxJr@users.noreply.github.com>
2020-10-13 15:44:10 -0400

    Revert "Revert "Modify Step 3,""
    This reverts commit ec2687fa1bb6104c1be9da58462c41d63cbf28ec.

---
+++ bankingApplication/bankingApplication/addOn/GlobalSavingAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankingApplication.addOn
+{
+    class GlobalSavingAccount : SavingAccount, IExchangeable
+    {
+        public GlobalSavingAccount(double sb, double ai) : base(sb, ai)
+        {
+            startingBalance = sb;
+            annualInterest = ai;
+        }
+
+        public double USValue (double rate)
+        {
+            return (currentBalance *= rate);
+        }
+    }
+}
